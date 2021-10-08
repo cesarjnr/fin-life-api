ea09b04dd7fd8c26c1b62c54313e495d38d47453
César Júnior <cesar.jnr@hotmail.com>
2021-10-07 21:41:32 -0300

    Finish the first migration

--- migrations/1633236647888-create_users_table.ts
+++ migrations/1633236647888-create_users_table.ts
@@ -15,11 +15,21 @@ export class createUsersTable1633236647888 implements MigrationInterface {
         {
           name: 'name',
           type: 'varchar'
+        },
+        {
+          name: 'email',
+          type: 'varchar'
+        },
+        {
+          name: 'password',
+          type: 'varchar',
+          length: '16'
         }
       ]
     }))
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
   }
 }
