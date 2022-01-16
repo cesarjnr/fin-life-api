8bdf70113010047252dcafedb6564ed9f2199e73
César Júnior <cesar.jnr@hotmail.com>
2022-01-16 12:11:08 -0300

    Write expense category tests

--- src/expenseCategories/expenseCategories.service.spec.ts
+++ src/expenseCategories/expenseCategories.service.spec.ts
@@ -1,10 +1,13 @@
+import * as faker from 'faker';
 import { Repository } from 'typeorm';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 import { User } from '../users/user.entity';
 import { ExpenseCategoriesService } from './expenseCategories.service';
 import { ExpenseCategory } from './expenseCategory.entity';
+import { CreateExpenseCategoryDto } from './createExpenseCategory.dto';
 
 describe('ExpenseCategoriesService', () => {
   let mockUsersRepository: jest.Mocked<Repository<User>>;
@@ -35,5 +38,50 @@ describe('ExpenseCategoriesService', () => {
       getRepositoryToken(ExpenseCategory)
     );
     expenseCategoriesService = moduleRef.get<ExpenseCategoriesService>(ExpenseCategoriesService);
+  });
+
+  describe('create', () => {
+    const createExpenseCategoryDto: CreateExpenseCategoryDto = {
+      userId: faker.datatype.number(100),
+      description: faker.lorem.paragraph(),
+      revenuePercentage: faker.datatype.number(100)
+    };
+
+    it('should throw an NotFoundException when no user is found for the given userId', async () => {
+      const thrownError = new NotFoundException('User not found');
+
+      mockUsersRepository.findOne = jest.fn();
+      mockExpenseCategoriesRepository.save = jest.fn();
+
+      await expect(expenseCategoriesService.create(createExpenseCategoryDto)).rejects.toStrictEqual(thrownError);
+      expect(mockUsersRepository.findOne).toHaveBeenCalledWith(createExpenseCategoryDto.userId);
+      expect(mockExpenseCategoriesRepository.save).not.toHaveBeenCalled();
+    });
+
+    it('should create an expense category', async () => {
+      const user = new User({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(16)
+      });
+      const newExpenseCategoryId = faker.datatype.number(100);
+
+      user.id = faker.datatype.number(100);
+      mockUsersRepository.findOne = jest.fn().mockResolvedValue(user);
+      mockExpenseCategoriesRepository.save = jest.fn().mockImplementation((expenseCategory: ExpenseCategory) => {
+        expenseCategory.id = newExpenseCategoryId;
+
+        return expenseCategory;
+      });
+
+      const newExpenseCategory = await expenseCategoriesService.create(createExpenseCategoryDto);
+
+      expect(newExpenseCategory).toEqual({
+        ...createExpenseCategoryDto,
+        id: newExpenseCategoryId
+      });
+      expect(mockUsersRepository.findOne).toHaveBeenCalledWith(createExpenseCategoryDto.userId);
+      expect(mockExpenseCategoriesRepository.save).toHaveBeenCalled();
+    });
   });
 });
--- src/expenseCategories/expenseCategories.service.ts
+++ src/expenseCategories/expenseCategories.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 
-import { CreateExpenseCategoryDto } from './createExpenseCategory.dto';
+import { User } from '../users/user.entity';
 import { ExpenseCategory } from './expenseCategory.entity';
+import { CreateExpenseCategoryDto } from './createExpenseCategory.dto';
 
 @Injectable()
 export class ExpenseCategoriesService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(ExpenseCategory) private expenseCategoriesRepository: Repository<ExpenseCategory>
+  ) {}
+
   public async create(createExpenseCategoryDto: CreateExpenseCategoryDto): Promise<ExpenseCategory> {
-    return {} as ExpenseCategory;
+    await this.checkIfUserExists(createExpenseCategoryDto.userId);
+
+    const newExpenseCategory = new ExpenseCategory(createExpenseCategoryDto);
+
+    await this.expenseCategoriesRepository.save(newExpenseCategory);
+
+    return newExpenseCategory;
+  }
+
+  private async checkIfUserExists(userId: number): Promise<void> {
+    const user = await this.usersRepository.findOne(userId);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
   }
 }
